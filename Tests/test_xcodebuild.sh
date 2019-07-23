9d82a0d15e499352313670eebffc0560565ac72b
Andrew Wagner <845683+drewag@users.noreply.github.com>
2019-07-23 15:49:39 -0600

    [CI] Fix CI tests

--- Tests/test_xcodebuild.sh
+++ Tests/test_xcodebuild.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-set -e 
+set -e
 set -o pipefail
 
 sudo xcode-select --switch /Applications/$1.app/Contents/Developer
 
 xcodebuild -version
-xcodebuild build -scheme XMLCoder \
+xcodebuild build -scheme Decree \
   -sdk iphonesimulator -destination "$IOS_DEVICE" | xcpretty
-xcodebuild build -scheme XMLCoder \
+xcodebuild build -scheme Decree \
   -sdk appletvsimulator -destination "$TVOS_DEVICE" | xcpretty
 
 if [ -n "$CODECOV_JOB" ]; then
-  xcodebuild test -enableCodeCoverage YES -scheme XMLCoder \
+  xcodebuild test -enableCodeCoverage YES -scheme Decree \
     -sdk macosx | xcpretty
   bash <(curl -s https://codecov.io/bash)
-else 
-  xcodebuild test -scheme XMLCoder \
+else
+  xcodebuild test -scheme Decree \
     -sdk macosx | xcpretty
 fi
