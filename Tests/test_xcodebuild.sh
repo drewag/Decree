6ab1b7ce85b86402fabca005a96958f3c6df4872
Andrew Wagner <845683+drewag@users.noreply.github.com>
2019-07-23 15:55:18 -0600

    [CI] Fix Xcode10 azure CI and remove pod

--- Tests/test_xcodebuild.sh
+++ Tests/test_xcodebuild.sh
@@ -6,16 +6,16 @@ set -o pipefail
 sudo xcode-select --switch /Applications/$1.app/Contents/Developer
 
 xcodebuild -version
-xcodebuild build -scheme Decree \
+xcodebuild build -scheme Decree-Package \
   -sdk iphonesimulator -destination "$IOS_DEVICE" | xcpretty
-xcodebuild build -scheme Decree \
+xcodebuild build -scheme Decree-Package \
   -sdk appletvsimulator -destination "$TVOS_DEVICE" | xcpretty
 
 if [ -n "$CODECOV_JOB" ]; then
-  xcodebuild test -enableCodeCoverage YES -scheme Decree \
+  xcodebuild test -enableCodeCoverage YES -scheme Decree-Package \
     -sdk macosx | xcpretty
   bash <(curl -s https://codecov.io/bash)
 else
-  xcodebuild test -scheme Decree \
+  xcodebuild test -scheme Decree-Package \
     -sdk macosx | xcpretty
 fi
