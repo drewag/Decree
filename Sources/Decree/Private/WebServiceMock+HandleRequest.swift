fc8074159bb266f97cdf61dc3217c4ac2c9cc67f
Andrew Wagner <845683+drewag@users.noreply.github.com>
2019-08-17 18:58:36 -0600

    [CI] More accurately mock the threading of mocked requests
    Do input processing on the calling thread (just like real requests),
    then move through a background queue to simulate the network request.

--- Sources/Decree/Private/WebServiceMock+HandleRequest.swift
+++ Sources/Decree/Private/WebServiceMock+HandleRequest.swift
@@ -9,82 +9,90 @@ import Foundation
 
 extension WebServiceMock {
     func handle<E: EmptyEndpoint>(for endpoint: E, callbackQueue: DispatchQueue?, onComplete: @escaping (_ result: EmptyResult) -> ()) {
-        callbackQueue.async {
-            do {
-                let next = try self.nextExpectation(for: endpoint)
-                switch next {
-                case let expectation as EmptyExpectation<E>:
-                    try expectation.validate(path: endpoint.path, for: endpoint)
-                    try expectation.validate(endpoint)
-                    onComplete(expectation.returning)
-                    expectation.fulfill()
-                default:
-                    throw DecreeError(.incorrectExpecation(expected: type(of: next).typeName, actual: "\(E.self)"), operationName: E.operationName)
+        do {
+            let next = try self.nextExpectation(for: endpoint)
+            switch next {
+            case let expectation as EmptyExpectation<E>:
+                try expectation.validate(path: endpoint.path, for: endpoint)
+                try expectation.validate(endpoint)
+                DispatchQueue(label: "background").async {
+                    callbackQueue.async {
+                        onComplete(expectation.returning)
+                        expectation.fulfill()
+                    }
                 }
+            default:
+                throw DecreeError(.incorrectExpecation(expected: type(of: next).typeName, actual: "\(E.self)"), operationName: E.operationName)
             }
-            catch {
-                onComplete(.failure(DecreeError(other: error, for: endpoint)))
-            }
+        }
+        catch {
+            onComplete(.failure(DecreeError(other: error, for: endpoint)))
         }
     }
 
     func handle<E: InEndpoint>(for endpoint: E, input: E.Input, callbackQueue: DispatchQueue?, onComplete: @escaping (_ result: EmptyResult) -> ()) where E.Input: Encodable {
-        callbackQueue.async {
-            do {
-                let next = try self.nextExpectation(for: endpoint)
-                switch next {
-                case let expectation as InExpectation<E>:
-                    try expectation.validate(path: endpoint.path, for: endpoint)
-                    try expectation.validate(endpoint, input: input)
-                    onComplete(expectation.returning)
-                    expectation.fulfill()
-                default:
-                    throw DecreeError(.incorrectExpecation(expected: type(of: next).typeName, actual: "\(E.self)"), operationName: E.operationName)
+        do {
+            let next = try self.nextExpectation(for: endpoint)
+            switch next {
+            case let expectation as InExpectation<E>:
+                try expectation.validate(path: endpoint.path, for: endpoint)
+                try expectation.validate(endpoint, input: input)
+                DispatchQueue(label: "background").async {
+                    callbackQueue.async {
+                        onComplete(expectation.returning)
+                        expectation.fulfill()
+                    }
                 }
+            default:
+                throw DecreeError(.incorrectExpecation(expected: type(of: next).typeName, actual: "\(E.self)"), operationName: E.operationName)
             }
-            catch {
-                onComplete(.failure(DecreeError(other: error, for: endpoint)))
-            }
+        }
+        catch {
+            onComplete(.failure(DecreeError(other: error, for: endpoint)))
         }
     }
 
     func handle<E: OutEndpoint>(for endpoint: E, callbackQueue: DispatchQueue?, onComplete: @escaping (_ result: Result<E.Output, DecreeError>) -> ()) {
-        callbackQueue.async {
-            do {
-                let next = try self.nextExpectation(for: endpoint)
-                switch next {
-                case let expectation as OutExpectation<E>:
-                    try expectation.validate(path: endpoint.path, for: endpoint)
-                    try expectation.validate(endpoint)
-                    onComplete(expectation.returning)
-                    expectation.fulfill()
-                default:
-                    throw DecreeError(.incorrectExpecation(expected: type(of: next).typeName, actual: "\(E.self)"), operationName: E.operationName)
+        do {
+            let next = try self.nextExpectation(for: endpoint)
+            switch next {
+            case let expectation as OutExpectation<E>:
+                try expectation.validate(path: endpoint.path, for: endpoint)
+                try expectation.validate(endpoint)
+                DispatchQueue(label: "background").async {
+                    callbackQueue.async {
+                        onComplete(expectation.returning)
+                        expectation.fulfill()
+                    }
                 }
+            default:
+                throw DecreeError(.incorrectExpecation(expected: type(of: next).typeName, actual: "\(E.self)"), operationName: E.operationName)
             }
-            catch {
-                onComplete(.failure(DecreeError(other: error, for: endpoint)))
-            }
+        }
+        catch {
+            onComplete(.failure(DecreeError(other: error, for: endpoint)))
         }
     }
 
     func handle<E: InOutEndpoint>(for endpoint: E, input: E.Input, callbackQueue: DispatchQueue?, onComplete: @escaping (_ result: Result<E.Output, DecreeError>) -> ()) where E.Input: Encodable {
-        callbackQueue.async {
-            do {
-                let next = try self.nextExpectation(for: endpoint)
-                switch next {
-                case let expectation as InOutExpectation<E>:
-                    try expectation.validate(path: endpoint.path, for: endpoint)
-                    try expectation.validate(endpoint, input: input)
-                    onComplete(expectation.returning)
-                    expectation.fulfill()
-                default:
-                    throw DecreeError(.incorrectExpecation(expected: type(of: next).typeName, actual: "\(E.self)"), operationName: E.operationName)
+        do {
+            let next = try self.nextExpectation(for: endpoint)
+            switch next {
+            case let expectation as InOutExpectation<E>:
+                try expectation.validate(path: endpoint.path, for: endpoint)
+                try expectation.validate(endpoint, input: input)
+                DispatchQueue(label: "background").async {
+                    callbackQueue.async {
+                        onComplete(expectation.returning)
+                        expectation.fulfill()
+                    }
                 }
+            default:
+                throw DecreeError(.incorrectExpecation(expected: type(of: next).typeName, actual: "\(E.self)"), operationName: E.operationName)
             }
-            catch {
-                onComplete(.failure(DecreeError(other: error, for: endpoint)))
-            }
+        }
+        catch {
+            onComplete(.failure(DecreeError(other: error, for: endpoint)))
         }
     }
 }
