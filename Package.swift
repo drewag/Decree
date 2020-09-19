dc5638d1a30595a0d65e59fd950f34a982b0cec0
Andrew Wagner <845683+drewag@users.noreply.github.com>
2020-09-19 13:16:07 -0600

    [UP] Update to latest XMLCoder

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Decree",
     platforms: [
-        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9),
+        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v4)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
